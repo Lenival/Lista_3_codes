d617c12f206281d5993eabb9a059dc2935bb4ebe
Danilo Pena <danilopena@dhcp156.dca.ufrn.br>
2013-05-18 10:52:49 -0300

    Adicionado testes para cada arquivo .m.

--- algoritmos_implementados/testes.m
+++ algoritmos_implementados/testes.m
@@ -1,4 +1,4 @@
-% Teste do BP_MLP
+%% Teste do BP_MLP.m
 clear
 
 rede = BP_MLP(2,[4 1],'tgh')
@@ -7,4 +7,12 @@ entradas = [1 1 -1; -1 -1 -1; -1 1 1; 1 -1 1]
 
 rede.treinar(entradas,100,0.4,0.01)
 
-%[w e] = BP(2, [4 1], 'tgh', entradas, 100, 0.4, 0.01);
+%% Teste do BP.m
+[w e] = BP(2, [4 1], 'tgh', entradas, 100, 0.4, 0.01);
+
+%% Teste do backpropagation.m
+x = [1 1; -1 -1; -1 1; 1 -1];
+d = [-1; -1; 1; 1];
+arq = [4 1];
+
+[w e] = backpropagation(x, d, arq);
