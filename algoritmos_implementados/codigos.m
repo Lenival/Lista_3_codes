c28b7acef5ef631af9a165c7da02bfcd07a00af3
comesanha <raphael.comesanha@gmail.com>
2013-05-22 08:28:51 -0300

    codigos.m: realizei pequenos ajustes

--- algoritmos_implementados/codigos.m
+++ algoritmos_implementados/codigos.m
@@ -33,12 +33,11 @@ c2 = (rand(2,10)*.2)+repmat([0.5;1],1,10);
 a1 = [c1 c2];
 c3 = (rand(2,10)*.2)+repmat([-1;0.5],1,10);
 c4 = (rand(2,10)*.2)+repmat([0.5;1],1,10);
-a2 = [c3 c4];
+a2 = [c4 c3];
 %plot (a(1,:),a(2,:),'*')
 %grid on
 
 redet = ALG_COMPET(2,20,2);
-<<<<<<< HEAD
 redet.nor(a1)
 figure
 md = redet.trei(0.1,1000);
