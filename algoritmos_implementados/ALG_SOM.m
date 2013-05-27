63403977ffe3341e4e17bd24a02d38a54f135350
comesanha <raphael.comesanha@gmail.com>
2013-05-27 14:38:27 -0300

    algoritmo SOM

---
+++ algoritmos_implementados/ALG_SOM.m
@@ -0,0 +1,112 @@
+classdef ALG_SOM < handle
+    properties
+        xi % matriz de amostras de treinamento
+        xin % vetor de entrada do k-ésimo padrão
+        nn % Matriz do mapa de neurônios
+        w % matriz do pesos 
+        wn % vetro normalizado de pesos
+        eta % taxa de aprendizagem
+        lx % nº de linhas das matizes de pesos e de treinamento (nº de entrdadas)
+        cx % nº de colunas da matriz de amostras de treinamneto (nº de amostras)
+        cw % nº de colunas da matriz de pesos (nº de neurônios)
+    end
+    methods
+        function obj = ALG_SOM(n_ent,n_amost, n_neu)
+            % inicialização da matriz de pesos e de amostaras
+            obj.xi = -1*ones(n_ent+1, n_amost);% O +1 é para o bias -1
+            obj.nn = 
+            obj.w = rand(n_ent+1,n_neu); 
+            nw = ceil(0.2*n_amost);
+            sxi = 0;
+            for j = 1:n_neu
+                for i = 1:n_ent
+                    for k = 1:nw
+                        sxi = sxi + obj.xi(i,k);
+                    end
+                    obj.w(i,j) = (sxi/nw)*.01*j;
+                end
+            end
+            obj.lx = n_ent+1;% nº de entradas
+            obj.cx = n_amost;% nº de amostras
+            obj.cw = n_neu;% nº de neurônios
+        end
+        function nor(obj,entradat)
+            obj.xi(2:obj.lx,:) = entradat;
+            % Normalização dos vetores de pesos e de treinamento
+             for xc = 1:obj.cx
+                 obj.xin(:,xc) = obj.xi(:,xc)/norm(obj.xi(:,xc));
+             end
+             for wc = 1:obj.cw
+                 obj.wn(:,wc) = obj.w(:,wc)/norm(obj.w(:,wc));
+             end
+            plot3(obj.wn(1,:),obj.wn(2,:),obj.wn(3,:),'*r')
+            hold on
+            plot3(obj.xin(1,:),obj.xin(2,:),obj.xin(3,:),'*')
+            sphere            
+            grid
+        end
+        function [mdist W] = trei(obj,txap,nep)
+            obj.eta = txap; %Taxa de aprendizagem
+            a=0;
+            dist = [];
+            mdist = [];
+            ind = 0;
+            epoca = 1;
+            while a==0
+                a=0;
+                for k = 1:obj.cx
+                    for j = 1:obj.cw
+                        somatxw = 0;
+                        for i = 1:obj.lx
+                            somatxw = somatxw + (obj.xin(i,k)-obj.wn(i,j))^2;
+                        end
+                        dist(j) = sqrt(somatxw);
+                    end
+                    [mdist(k,epoca) ind] = min(dist);
+                    obj.wn(:,ind) = obj.wn(:,ind)+ obj.eta*(obj.xin(:,k)-obj.wn(:,ind));
+                end
+                if epoca == nep
+                    a=1;
+                end
+                epoca=epoca+1;
+            end
+            plot3(obj.wn(1,:),obj.wn(2,:),obj.wn(3,:),'*r')
+            hold on
+            plot3(obj.xin(1,:),obj.xin(2,:),obj.xin(3,:),'*')
+            grid
+            W = obj.wn;
+        end
+        function ind = valid(obj, entradav, pesost)
+            obj.xi(2:obj.lx,:) = entradav;
+            % Normalização dos vetores de pesos e de treinamento
+            for xc = 1:obj.cx
+                obj.xin(:,xc) = obj.xi(:,xc)/norm(obj.xi(:,xc));
+            end
+            obj.wn = pesost;
+            plot3(obj.wn(1,:),obj.wn(2,:),obj.wn(3,:),'*r')
+            hold on
+            plot3(obj.xin(1,:),obj.xin(2,:),obj.xin(3,:),'*')
+            grid
+            figure
+            for k = 1:obj.cx
+                    for j = 1:obj.cw
+                        somatxw = 0;
+                        for i = 1:obj.lx
+                            somatxw = somatxw + (obj.xin(i,k)-obj.wn(i,j))^2;
+                        end
+                        dist(j) = sqrt(somatxw);
+                    end
+                    [mdist ind(k)] = min(dist);
+                    if ind == 1
+                         plot3(obj.xin(1,k),obj.xin(2,k),obj.xin(3,k),'*k')
+                    else
+                        plot3(obj.xin(1,k),obj.xin(2,k),obj.xin(3,k),'*r')
+                    end
+                    hold on    
+            end
+            plot3(obj.wn(1,:),obj.wn(2,:),obj.wn(3,:),'*y')
+            grid
+         end
+    end
+    
+end
