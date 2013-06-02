f282cc4607adb07c15e4874574eaa9551e982ac0
Danilo Pena <danilopena@dhcp154.dca.ufrn.br>
2013-06-02 11:31:49 -0300

    Lista 3.

--- algoritmos_implementados/codigos.m
+++ algoritmos_implementados/codigos.m
@@ -25,6 +25,35 @@ alpha = 0.01;
 
 [w e] = backpropagation(x, d, arq, eta, alpha);
 
+%% Teste do EKF
+
+n = 3;                  % Numero de estados
+dp = 0.1;               % Desvio padrao do processo
+r = 0.1;                % Desvio padrao da medida
+Q = dp^2*eye(n);        % Covariancia do processo
+R = r^2;                % Covariancia da medida
+f = @(x)[x(2);x(3);0.05*x(1)*(x(2)+x(3))];  % Equacoes de estados nao lineares
+h = @(x)x(1);                               % Equacoes de medidas
+s = [0;0;1];                                % Estado inicial
+x = s + dp*randn(3,1);                      % Estado inicial com ruido
+P = eye(n);                                 % Estado inicial de covariancia
+N = 20;                                     % Numero total de passos
+xV = zeros(n,N);                            % Estado estimado
+sV = zeros(n,N);                            % Estado atual
+zV = zeros(1,N);
+for k=1:N
+  z = h(s) + r*randn;                       % Medidas
+  sV(:,k)= s;                               % Estado atual armazenado
+  zV(k)  = z;                               % Medida armazenada
+  [x, P] = ekf(f,x,P,h,z,Q,R);              % EKF
+  xV(:,k) = x;                              % Estimacao armazenada
+  s = f(s) + dp*randn(3,1);                 % Processo atualizado
+end
+for k=1:3                                   % Plotando resultados
+  subplot(3,1,k)
+  plot(1:N, sV(k,:), '-', 1:N, xV(k,:), '--')
+end
+
 %% Teste do algoritmo competitivo
 clear
 close all
